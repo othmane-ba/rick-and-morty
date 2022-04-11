dbba4e7ae23cebab5cd22c9edb8fe6181b3d0076
othmane-ba <othmane.bakkas@hotmail.com>
2022-04-11 10:29:49 +0000

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import '../styles/globals.css';
+import { motion, AnimatePresence } from 'framer-motion';
+
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <AnimatePresence>
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        exit="pageExit"
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+          pageExit: {
+            backgroundColor: 'white',
+            filter: `invert()`,
+            opacity: 0,
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </AnimatePresence>
+  );
+}
+
+export default MyApp;
